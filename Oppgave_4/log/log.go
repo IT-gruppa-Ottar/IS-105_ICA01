0b86abdaff5617372d281d2a55684e37cf956f75
Christoffer Lislevand <chrirl16@student.uia.no>
2017-05-12 12:12:32 +0200

    Oppgave_4 med kommentarer

--- Oppgave_4/log/log.go
+++ Oppgave_4/log/log.go
@@ -5,6 +5,10 @@ import (
 	"math"
 )
 
+/**
+For å beregne logaritmen til 2 må vi importere pakken "math".
+Dette tillater GetLog() å bruke "math.Log2"
+ */
 func GetLog() {
 	fmt.Println(math.Log2(2))
 }
